1e3cbf09155a077009619992665294bccc20ab7c
Dunor <dunor@mail.ru>
2019-07-13 21:29:04 +0700

    1.3. JavaDoc. Code convention[#158408]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,6 +1,15 @@
 package ru.job4j.calculate;
 
+/**
+ * Class Calculate выводит в консоль текст Hello, Job4j.
+ * @autor Максим К.
+ */
 public class Calculate {   
+
+   /**
+    * Выводит строку в консоль.
+    * @param args - args.
+    */
    public static void main(String[] args) {
       System.out.println("Hello, Job4j.");
    }
