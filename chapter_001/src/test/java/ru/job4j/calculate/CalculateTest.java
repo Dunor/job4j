93d49dd1938eba055fa61f7639beddda76ecbde1
Dunor <dunor@mail.ru>
2019-07-15 22:24:34 +0700

    1.4. JUnit[#158409]

---
+++ chapter_001/src/test/java/ru/job4j/calculate/CalculateTest.java
@@ -0,0 +1,28 @@
+package ru.job4j.calculate;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Test.
+ *
+ * @author Maksim K.
+ * @version $Id$
+ * @since 15.07.2019
+ */
+public class CalculateTest {
+ /**
+ * Test echo.
+ */
+
+@Test
+ public void whenTakeNameThenThreeEchoPlusName() {
+    String input = "Maksim";
+    String expect = "Echo, echo, echo : Maksim"; 
+    Calculate calc = new Calculate();
+    String result = calc.echo(input);
+    assertThat(result, is(expect));
+ }
+
+}
---
+++ chapter_001/src/test/java/ru/job4j/calculate/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Package for calculate task.
+ *
+ * @author Maxim kudryashov (dunor@mail.ru)
+ * @version $Id$
+ * @since 13.07.2019
+ */
+package ru.job4j.calculate;
