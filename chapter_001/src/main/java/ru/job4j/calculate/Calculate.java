2bcb8259a2802b974029ca634f7b47c6306b42b5
Dunor <dunor@mail.ru>
2019-07-22 14:14:27 +0700

    3.1. Элементарный калькулятор.[#158399]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,25 +1,64 @@
 package ru.job4j.calculate;
 
 /**
- * Class Calculate выводит в консоль текст Hello, Job4j.
+ * Class Calculate выполняет арифметические действия(сложение, вычетание, умножение, деление),
+ * и выводит результаты в консоль.
  * @author Максим К.
  */
 public class Calculate {   
 
    /**
-    * Выводит строку в консоль.
     * @param args - args.
     */
    public static void main(String[] args) {
-      System.out.println("Hello, Job4j.");
+      add(1, 1 );
+      div(4, 2);
+      multiply(2, 1);
+      subtrack(10, 5);
    }
-   
+
    /**
-    * Method echo.
-    * @param name Your name.
-    * @return Echo plus your name.
+    * Метод add складывает значения параметров first и second
+    * выводит результат в консоль
+    * @param first - первое слагаемое
+    * @param second - второе слагаемое
     */
-   public String echo(String name) {
-	   return "Echo, echo, echo : " + name;
- }
+   public static void add(double first, double second) {
+      double result =  first + second;
+      System.out.println(first + " + " + second + " = " + result);
+   }
+
+   /**
+    * Метод div делит значение параметра first на second
+    * выводит результат в консоль
+    * @param first - делимое
+    * @param second - делитель
+    */
+   public static void div(double first, double second) {
+      double result =  first / second;
+      System.out.println(first + " / " + second + " = " + result);
+   }
+
+   /**
+    * Метод multiply перемножает значения параметров first и second
+    * выводит результат в консоль
+    * @param first - первый множетель
+    * @param second - второй множетель
+    */
+   public static void multiply(double first, double second) {
+      double result =  first * second;
+      System.out.println(first + " * " + second + " = " + result);
+   }
+
+   /**
+    * Метод subtrack вычитает из значения параметра first значение second
+    * выводит результат в консоль
+    * @param first - уменьшаемое
+    * @param second - вычитаемое
+    */
+   public static void subtrack(double first, double second) {
+      double result =  first - second;
+      System.out.println(first + " - " + second + " = " + result);
+   }
+
 }
