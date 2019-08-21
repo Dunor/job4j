8c9fa335ccfa243d030aacd25b042c4d5db53e8e
Dunor <dunor@mail.ru>
2019-08-21 16:14:55 +0700

    # This is a combination of 2 commits. # This is the 1st commit message:
    1. Реализация профессий в коде[#158484]
    
    # This is the commit message #2:
    
    1. Реализация профессий в коде[#158484]

---
+++ chapter_002/src/main/java/ru/job4j/Inheritance/Language.java
@@ -0,0 +1,8 @@
+package ru.job4j.Inheritance;
+
+public class Language extends Teacher{
+    private String whatLanguage;
+    private boolean nativeSpeaker;
+    public String getWhatLanguage() {return this.whatLanguage;}
+    public boolean getNativeSpeaker() {return this.nativeSpeaker;}
+}
