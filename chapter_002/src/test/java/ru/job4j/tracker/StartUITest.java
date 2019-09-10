11f9550a8a5bb34662c024df8ffc4a5dab060181
Dunor <dunor@mail.ru>
2019-09-10 21:15:55 +0700

    6. Доработать тесты в StartUITest проверяющие вывод данных в консоль.[#158461]

--- chapter_002/src/test/java/ru/job4j/tracker/StartUITest.java
+++ chapter_002/src/test/java/ru/job4j/tracker/StartUITest.java
@@ -1,14 +1,35 @@
 package ru.job4j.tracker;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.assertNull;
-import static org.junit.Assert.assertThat;
+import static org.junit.Assert.*;
 
 public class StartUITest {
+    // поле содержит дефолтный вывод в консоль.
+    private final PrintStream stdout = System.out;
+    // буфер для результата.
+    private final ByteArrayOutputStream out = new ByteArrayOutputStream();
+    private  Tracker tracker;
+
+    @Before
+    public void loadOutput() {
+        System.out.println("execute before method");
+        tracker = new Tracker();     // создаём Tracker
+    }
+
+    @After
+    public void backOutput() {
+        System.out.println("execute after method");
+    }
+
     @Test
     public void whenUserAddItemThenTrackerHasNewItemWithSameName() {
-        Tracker tracker = new Tracker();     // создаём Tracker
         Input input = new StubInput(new String[]{"0", "test name", "desc", "6"});   //создаём StubInput с последовательностью действий
         new StartUI(input, tracker).init();     //   создаём StartUI и вызываем метод init()
         assertThat(tracker.findAll()[0].getName(), is("test name")); // проверяем, что нулевой элемент массива в трекере содержит имя, введённое при эмуляции.
@@ -16,8 +37,6 @@ public class StartUITest {
 
     @Test
     public void whenUpdateThenTrackerHasUpdatedValue() {
-        // создаём Tracker
-        Tracker tracker = new Tracker();
         //Напрямую добавляем заявку
         Item item = tracker.add(new Item("test name", "desc"));
         //создаём StubInput с последовательностью действий(производим замену заявки)
@@ -30,11 +49,45 @@ public class StartUITest {
 
     @Test
     public void whenUserDeleteItemByID() {
-        Tracker tracker = new Tracker();
         Item item = tracker.add(new Item("test name", "desc"));
         String id = item.getId();
         Input input = new StubInput(new String[]{"3", id, "заявка удалена", "6"});
         new StartUI(input, tracker).init();
         assertNull(tracker.findById(id));
+    }
+
+    @Test
+    public void whenShowAllItems() {
+        Item item = tracker.add(new Item("test1", "desc1"));
+        System.setOut(new PrintStream(this.out));
+        Input input = new StubInput(new String[]{"1", "6"});
+        new StartUI(input, tracker).init();
+        assertTrue("",new String(out.toByteArray()).indexOf(new StringBuilder()
+                        .append("-------- Заявка --------\r\n")
+                        .append("ID: " + tracker.findAll()[0].getId() + "\r\n")
+                        .append("Название: " + tracker.findAll()[0].getName() + "\r\n")
+                        .append("Описание: " + tracker.findAll()[0].getDesc() + "\r\n")
+                        .append(System.lineSeparator())
+                        .toString()) != -1
+        );
+        System.setOut(this.stdout);
+    }
+
+    @Test
+    public void whenUserFindItemByName(){
+        Item item = tracker.add(new Item("test1", "desc1"));
+        String name = item.getName();
+        System.setOut(new PrintStream(this.out));
+        Input input = new StubInput(new String[]{"5", name, "6"});
+        new StartUI(input, tracker).init();
+        assertTrue("",new String(out.toByteArray()).indexOf(new StringBuilder()
+                .append("-------- Заявка --------\r\n")
+                .append("ID: " + tracker.findAll()[0].getId() + "\r\n")
+                .append("Название: " + name + "\r\n")
+                .append("Описание: " + tracker.findAll()[0].getDesc() + "\r\n")
+                .append(System.lineSeparator())
+                .toString()) != -1
+        );
+        System.setOut(this.stdout);
     }
 }
