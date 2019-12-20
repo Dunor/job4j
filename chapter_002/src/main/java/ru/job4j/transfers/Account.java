c53f2b7004a0fea8193371f0b3adbb409e7e3c70
Dunor <dunor@mail.ru>
2019-12-20 14:27:38 +0700

    Банковские переводы.[#158429]

---
+++ chapter_002/src/main/java/ru/job4j/transfers/Account.java
@@ -0,0 +1,27 @@
+package ru.job4j.transfers;
+
+public class Account {
+    private double value;
+    private String requisite;
+
+    public Account(double value, String requisite) {
+        this.value = value;
+        this.requisite = requisite;
+    }
+
+    public double getValue() {
+        return value;
+    }
+
+    public void setValue(double value) {
+        this.value = value;
+    }
+
+    public String getRequisites() {
+        return requisite;
+    }
+
+    public void setRequisites(String requisite) {
+        this.requisite = requisite;
+    }
+}
---
+++ chapter_002/src/main/java/ru/job4j/transfers/BankTransfers.java
@@ -0,0 +1,86 @@
+package ru.job4j.transfers;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class BankTransfers {
+    private List<Account> accounts = new ArrayList<>();
+    private Map<User, List<Account>> clients = new HashMap<>();
+
+    //добавление пользователя.
+    public void addUser(User user) {
+        clients.putIfAbsent(user,  accounts);
+    }
+
+    // удаление пользователя.
+    public void deleteUser(User user) {
+        for (Map.Entry<User, List<Account>> entry : clients.entrySet()) {
+            if (entry.getKey().equals(user)) {
+                clients.remove(user);
+            }
+        }
+    }
+
+    //добавить счёт пользователю.
+    public void addAccountToUser(String passport, Account account) {
+        for (Map.Entry<User, List<Account>> entry : clients.entrySet()) {
+            if (entry.getKey().getPassport().equals(passport)) {
+                entry.getValue().add(account);
+            }
+        }
+
+    }
+
+    //удалить один счёт пользователя.
+    public void deleteAccountFromUser(String passport, Account account) {
+        for (Map.Entry<User, List<Account>> entry : clients.entrySet()) {
+            if (entry.getKey().getPassport().equals(passport)) {
+                entry.getValue().remove(account);
+            }
+        }
+
+    }
+
+    //получить список счетов для пользователя.
+    public List<Account> getUserAccounts (String passport) {
+        List<Account> list = null;
+        for (Map.Entry<User, List<Account>> entry : clients.entrySet()) {
+            if (entry.getKey().getPassport().equals(passport)) {
+                list = entry.getValue();
+            }
+        }
+        return list;
+    }
+
+    //метод для перечисления денег с одного счёта на другой счёт
+    public boolean transferMoney (String srcPassport, String srcRequisite,
+                                  String destPassport, String dstRequisite,
+                                  double amount) {
+        boolean result = false;
+        Account srcAccount = null;
+        Account dstAccount = null;
+
+        for (Map.Entry<User, List<Account>> entry : clients.entrySet()) {
+            if (entry.getKey().getPassport().equals(srcPassport)
+            && entry.getValue().indexOf(srcRequisite) != -1) {
+                srcAccount = entry.getValue().get(entry.getValue().indexOf(srcRequisite));
+            }
+            if (entry.getKey().getPassport().equals(destPassport)
+                        && entry.getValue().indexOf(dstRequisite) != -1) {
+                            dstAccount = entry.getValue().get(entry.getValue().indexOf(dstRequisite));
+            }
+        }
+        if (srcAccount != null && dstAccount != null && srcAccount.getValue() > amount) {
+            dstAccount.setValue(dstAccount.getValue() + amount);
+            srcAccount.setValue(srcAccount.getValue() - amount);
+            result = true;
+        }
+        return result;
+    }
+
+    public Map<User, List<Account>> getClients() {
+        return clients;
+    }
+}
